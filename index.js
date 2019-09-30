1ef71502d239129e3c4896244f52195976eb48f6
saja amer <sajaamer86@gmail.com>
2019-09-30 06:19:44 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const farmAnimals = 'cow horse sheep pig chicken'
 ////
 const [moo,neigh,baa,oink,cluck] = farmAnimals.split(' ');
-
+////
 const colors = ['red', 'orange', 'yellow', 'green', 'blue', 'indigo', 'violet']
 
 const muppet = {
